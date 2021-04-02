f9fc16823798d49454583b84568d209e48dbf3bd
Zifran <robinnlive@gmail.com>
2021-04-03 00:42:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function digitalClock(){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let timeFormat = 'AM';
+
+    if (hours===0){
+        hours = 12;
+    }
+    if ( hours > 12) {
+        hours = hours - 12;
+        timeFormat = 'PM';
+    }
+
+    // if ( hours < 10){
+    //     hours = '0'+ hours;
+    // }
+
+    hours = hours<10 ? '0' + hours : hours; // ternary oparator
+    minutes = minutes<10 ? '0' + minutes : minutes; 
+    seconds = seconds<10 ? '0' + seconds : seconds; 
+
+    let finalTime = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById('time').innerText = finalTime;
+    document.querySelector('small').innerText = timeFormat;
+    document.querySelector('h4').innerText = wish;
+
+    setInterval(digitalClock, 1000);
+}
+
+digitalClock();
